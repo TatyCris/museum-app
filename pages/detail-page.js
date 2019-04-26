7e5f0b307fabeb89d84517105af5224c63095e46
TATY <tatyccsilva@gmail.com>
2019-04-26 15:02:51 +0200

    Create elements with data from form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,5 +1,23 @@
-function submitComment () {
-    console.log("hello!!");
-}
+function submitComment() {
+    //Gather data 
+    const inputField = document.getElementById('name');
+    const name = inputField.value;
+    const textArea = document.getElementById('msg');  
+    const msg = textArea.value;
 
-submitComment();
+    //Create elements
+    const comment = document.createElement('section');
+    const h3 = document.createElement('h3');
+    const p = document.createElement('p');
+
+    //Adjust elements
+    h3.innerHTML = `${name} said:`;
+    p.innerHTML = msg;
+    comment.classList.add('comment');
+    comment.appendChild(h3);
+    comment.appendChild(p);
+
+    console.log(comment);
+    
+  }
+
